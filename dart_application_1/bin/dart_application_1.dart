265930d97d50dece6746c5f1ff430ccbf7fab9ec
tgsilva94 <tgsilva.fx@hotmail.com>
2022-08-29 11:09:34 -0300

    teste

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,6 +1,9 @@
 import 'package:dart_application_1/dart_application_1.dart'
     as dart_application_1;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Ola mundo: ${dart_application_1.calculate()}!');
+  print('qual é o seu nome ?');
+  String? nome = stdin.readLineSync()!;
+  print('Ola ' + nome);
 }
